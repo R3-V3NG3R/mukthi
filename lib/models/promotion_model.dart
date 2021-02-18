4844e8c397bc2551cd5f540a22256500c812e8bb
Vamsi <vamsikrishnakdml@gmail.com>
2021-02-18 21:43:35 +0530

    Added Promotion Model in promotion_model.dart

---
+++ lib/models/promotion_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+
+class PromotionModel {
+  @required
+  final String id;
+  @required
+  final String imageUrl;
+  @required
+  final String destination;
+  @required
+  final String accentColor;
+
+  PromotionModel(this.id, this.destination, this.imageUrl, this.accentColor);
+
+  PromotionModel.fromJson(Map<String, dynamic> data)
+      : id = data['id'],
+        imageUrl = data['imageUrl'],
+        destination = data['destination'],
+        accentColor = data["accentColor"];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'imageUrl': imageUrl,
+        'destination': destination,
+        'accentColor': accentColor,
+      };
+}
--- lib/screens/home_screen/home_Screen.dart
+++ lib/screens/home_screen/home_Screen.dart
@@ -1,7 +1,5 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flashy_tab_bar/flashy_tab_bar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_greetings/flutter_greetings.dart';
